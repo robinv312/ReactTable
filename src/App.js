16cd8b61bc1cea30b2597497c6c1b6a6f12bf22e
Robin Varghese <robinv312@gmail.com>
2020-11-11 11:53:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import { FilteringTable } from './components/FilteringTable';
 import { PaginationTable } from './components/PaginationTable';
 import { ColumnOrder } from './components/ColumnOrder';
 import { RowSelection } from './components/RowSelection';
+import { ColumnHiding } from './components/ColumnHiding';
+import { StickyTable } from './components/StickyTable';
 
 
 function App() {
   return (
     <div className="App">
-     <RowSelection/>
+     
+     <StickyTable/>
     </div>
   );
 }
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,88 +1,96 @@
-
-
 import { format } from 'date-fns'
-import { ColumnFilter } from './ColumnFilter'
 
 export const COLUMNS = [
-    {
-        Header: 'Id',
-        accessor: 'id',
-        Footer: 'Id',
-        disableFilters: true
-    },
-    {
-        Header: 'First Name',
-        accessor: 'first_name',
-        Footer: 'First Name'
-
-    },
-    {
-        Header: 'Last Name',
-        accessor: 'last_name',
-        Footer: 'Last Name'
-
-    },
-    {
-        Header: 'Date of Birth',
-        accessor: 'date_of_birth',
-        Footer: 'Date of Birth',
-        Cell: ({ value }) => { return format(new Date(value), 'dd/mm/yyyy') }
-    },
-    {
-        Header: 'Country',
-        accessor: 'country',
-        Footer: 'Country'
-    },
-    {
-        Header: 'Phone',
-        accessor: 'phone',
-        Footer: 'Phone'
+  {
+    Header: 'Id',
+    Footer: 'Id',
+    accessor: 'id',
+    disableFilters: true,
+    sticky: 'left'
+  },
+  {
+    Header: 'First Name',
+    Footer: 'First Name',
+    accessor: 'first_name',
+    sticky: 'left'
+  },
+  {
+    Header: 'Last Name',
+    Footer: 'Last Name',
+    accessor: 'last_name',
+    sticky: 'left'
+  },
+  {
+    Header: 'Date of Birth',
+    Footer: 'Date of Birth',
+    accessor: 'date_of_birth',
+    Cell: ({ value }) => {
+      return format(new Date(value), 'dd/MM/yyyy')
     }
+  },
+  {
+    Header: 'Country',
+    Footer: 'Country',
+    accessor: 'country'
+  },
+  {
+    Header: 'Phone',
+    Footer: 'Phone',
+    accessor: 'phone'
+  },
+  {
+    Header: 'Email',
+    Footer: 'Email',
+    accessor: 'email'
+  },
+  {
+    Header: 'Age',
+    Footer: 'Age',
+    accessor: 'age'
+  },
 ]
 
 export const GROUPED_COLUMNS = [
-    {
-        Header: 'Id',
-        accessor: 'id',
-        Footer: 'Id'
-    },
-    {
-        Header: 'Name',
-        Footer: 'Name',
-        columns: [
-            {
-                Header: 'First Name',
-                accessor: 'first_name',
-                Footer: 'First Name'
-            },
-            {
-                Header: 'Last Name',
-                accessor: 'last_name',
-                Footer: 'Last Name'
-            },
-        ]
-
-    },
-    {
-        Header: 'Info',
-        Footer: 'Info',
-        columns: [
-            {
-                Header: 'Date of Birth',
-                accessor: 'date_of_birth',
-                Footer: 'Date of Birth'
-            },
-            {
-                Header: 'Country',
-                accessor: 'country',
-                Footer: 'Country'
-            },
-            {
-                Header: 'Phone',
-                accessor: 'phone',
-                Footer: 'Phone'
-            }
-
-        ]
-    }
+  {
+    Header: 'Id',
+    Footer: 'Id',
+    accessor: 'id'
+  },
+  {
+    Header: 'Name',
+    Footer: 'Name',
+    columns: [
+      {
+        Header: 'First Name',
+        Footer: 'First Name',
+        accessor: 'first_name'
+      },
+      {
+        Header: 'Last Name',
+        Footer: 'Last Name',
+        accessor: 'last_name'
+      }
+    ]
+  },
+  {
+    Header: 'Info',
+    Footer: 'Info',
+    columns: [
+      {
+        Header: 'Date of Birth',
+        Footer: 'Date of Birth',
+        accessor: 'date_of_birth'
+      },
+      {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor: 'country'
+      },
+      {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone'
+      }
+    ]
+  }
 ]
